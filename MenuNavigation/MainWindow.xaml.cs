f7b7cdf5a9ceaf13004b42c96a5e37562b08b9c7
Guto <guto.granja@hotmail.com>
2020-03-04 22:08:00 -0300

    First Commit

---
+++ MenuNavigation/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuNavigation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void popUpLogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        
+        private void openMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            openMenuButton.Visibility = Visibility.Collapsed;
+            closeMenuButton.Visibility = Visibility.Visible;
+        }
+
+        private void closeMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            openMenuButton.Visibility = Visibility.Visible;
+            closeMenuButton.Visibility = Visibility.Collapsed;
+        }
+
+    }
+}
